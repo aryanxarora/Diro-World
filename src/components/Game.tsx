bd6d5ec3fa4504424a003400726c900b994f2423
aryanxarora <aryanxarora@gmail.com>
2024-02-24 13:32:08 -0500

    PWA theme

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,7 +4,7 @@ const Game = () => {
   return (
     <>
       <div className="rpgui-content">
-        <div className="rpgui-container framed w-full h-full flex flex-col justify-between items-center">
+        <div className="rpgui-container w-full h-full flex flex-col justify-between items-center py-10">
           <div className="rpgui-container framed-golden-2 w-[90%]">
             <h1>Diro's World</h1>
           </div>
