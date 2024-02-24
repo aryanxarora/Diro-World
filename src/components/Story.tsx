833961aa69ed75ecade30e7db9c9e98609eadad0
Kelly Vergara T <kellyvergara12@gmail.com>
2024-02-24 17:54:46 -0500

    Story props

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,16 +1,23 @@
-import React, { useState } from "react"
+import { useState } from "react"
 
-const Story = (options,name,story) => {  
-    const [name] = useState("Aryan");
-    const paragraph = "Once upon a time in the bustling city of San Francisco, there lived an extraordinary young man named Aryan. At the tender age of 23, he was already a brilliant Computer Science student at the prestigious University of California, Berkeley. However, his heart yearned for something more than just algorithms and coding languages. He had an insatiable fascination with mystical dragons, ancient creatures of legend that were said to possess immense wisdom and power.";
-    const options = useState(["Pathway 1","Pathway 2","Pathway 3"]);
+interface Level {
+    name: string,
+    story: string,
+    options: string[]
+}
+
+const Story = ({ options, name, story }: Level) => {
+    const [characterName] = useState(name || "Aryan"); 
+    const paragraph = story || "Once upon a time..."; 
+    const pathways = useState(options || ["Pathway 1", "Pathway 2", "Pathway 3"]); 
+  
 
     return (
     <>
       <div className="rpgui-content relative">
         <div className="rpgui-container w-full h-full flex flex-col items-center justify-between p-10">
           <div className="rpgui-container framed-golden-2 relative">
-            <h1>{name}'s' World</h1>
+            <h1>{characterName}'s' World</h1>
           </div>
           <div className="!text-[10px] rpgui-container framed-golden relative">
             <p>
@@ -18,7 +25,7 @@ const Story = (options,name,story) => {
             </p>
           </div>
           <div className="flex flex-col">
-          {options.map((option, index) => 
+          {pathways.map((option, index) => 
               (
                 <button key={index} className="rpgui-button golden">
                     <p className="rpgui-center">{option}</p>
