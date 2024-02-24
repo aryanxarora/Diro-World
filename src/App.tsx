50e8d0f1093ce21042eec95d86917018afefaea3
aryanxarora <aryanxarora@gmail.com>
2024-02-24 17:05:39 -0500

    App Shell and Fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { Suspense } from "react"
-import { Game, Loading } from "./components"
+import { lazy, Suspense, useEffect } from "react"
+import { Loading } from "./components"
+const Game = lazy(() => import("./components/Game"))
 
 const App = () => {
   return (
